0ea8abbbbc25592c1a85c09ee8cb2cb984f95eb6
karthiga19 <karthiga19>
2025-08-01 19:25:58 +0000

    cleanup

--- config.py
+++ config.py
@@ -1,18 +1,8 @@
 # Databricks notebook source
-#%pip install --upgrade --quiet databricks-sdk 
-
-# COMMAND ----------
-
-#dbutils.library.restartPython()
-
-# COMMAND ----------
-
 # DBTITLE 1,Edit this cell with your resource names
-#catalog_name = "agent_bricks_demo"
-#schema_name = "data_schema"
+catalog_name = "agent_bricks_demo"
+schema_name = "data_schema"
 
-catalog_name = "users"
-schema_name = "karthiga_mahali"
 
 # COMMAND ----------
 
